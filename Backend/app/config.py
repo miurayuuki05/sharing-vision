9ee8142ff33ec2aef8ac2dfba2a98d9c0a62ce87
Faiz Islamic Qowy <67276767+miurayuuki05@users.noreply.github.com>
2025-02-10 21:19:45 +0700

    Update config.py

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    DB_USER = os.getenv('DB_USER', 'root')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', 'Faizqowy1.')
+    DB_USER = os.getenv('DB_USER', 'user')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
     DB_HOST = os.getenv('DB_HOST', 'localhost')
     DB_PORT = os.getenv('DB_PORT', '3306')
-    DB_NAME = os.getenv('DB_NAME', 'db_sv')
+    DB_NAME = os.getenv('DB_NAME', 'db_name')
     
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
